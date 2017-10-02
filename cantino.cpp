7fc086d5963ed921b4b55f15bca683763491c25b
JdeH <info@qquick.org>
2017-10-02 11:17:18 +0200

    First version of source

---
+++ cantino.cpp
@@ -0,0 +1,185 @@
+/*
+====== Legal notices
+
+Copyright 2017 Jacques de Hooge, GEATEC engineering, www.geatec.com
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+  http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+#include <arduino.h>
+
+#include "cantino.h"
+
+namespace cantino {
+
+// ====== Streams
+
+SerialStream::SerialStream (HardwareSerial &serial):
+    serial (&serial)
+{}
+
+SerialStream &SerialStream::operator>> (char * const chars) {
+    for (int index = 0; ; index++) {
+        while (!serial->available ()) {  // Wait for input
+        }
+        chars [index] = serial->read ();
+        serial->print (chars [index]);
+        if (chars [index] == '\n') {
+            chars [index] = (char) 0;
+            break;
+        }
+        while (serial->available ()) {   // Clear Serial's internal buffer
+        }
+    }
+    return *this;
+}
+
+SerialStream &SerialStream::operator>> (bool &aBool) {
+     (*this) >> conversionBuffer;
+     aBool = strcmp (conversionBuffer, "true") == 0;
+     return *this;
+}
+
+SerialStream &SerialStream::operator>> (char &aChar) {   
+     (*this) >> conversionBuffer;
+     aChar = conversionBuffer [0];
+     return *this;
+}
+
+SerialStream &SerialStream::operator>> (unsigned char &anUChar) {
+    return (*this) >> int (anUChar);
+}
+
+SerialStream &SerialStream::operator>> (int &anInt) {
+     (*this) >> conversionBuffer;
+     anInt = atoi (conversionBuffer);
+     return *this;    
+}
+
+SerialStream &SerialStream::operator>> (unsigned int &anUInt) {
+    return (*this) >> long (anUInt);
+}
+
+SerialStream &SerialStream::operator>> (long &aLong) {
+     (*this) >> conversionBuffer;
+     aLong = atol (conversionBuffer);
+     return *this;    
+}
+
+SerialStream &SerialStream::operator>> (unsigned long &anULong) {
+     return (*this) >> long (anULong);    
+}
+
+SerialStream &SerialStream::operator>> (float &aFloat) {
+     (*this) >> conversionBuffer;
+     aFloat = atof (conversionBuffer);
+     return *this;    
+}
+SerialStream &SerialStream::operator<< (const char * const chars) {
+    serial->write (chars);
+    serial->flush ();
+    return *this;
+}
+
+SerialStream &SerialStream::operator<< (bool aBool) {
+    return (*this) << (aBool ? "true" : "false");
+}
+
+SerialStream &SerialStream::operator<< (char aChar) {
+    conversionBuffer [0] = aChar;
+    conversionBuffer [1] = 0;
+    return (*this) << conversionBuffer;
+}
+
+SerialStream &SerialStream::operator<< (unsigned char anUChar) {
+    return (*this) << int (anUChar);
+}
+
+SerialStream &SerialStream::operator<< (int anInt) {
+    itoa (anInt, conversionBuffer, 10);
+    return (*this) << conversionBuffer;
+}
+
+SerialStream &SerialStream::operator<< (unsigned int anUInt) {
+    return (*this) << long (anUInt);
+}
+
+SerialStream &SerialStream::operator<< (long aLong) {
+    ltoa (aLong, conversionBuffer, 10);
+    return (*this) << conversionBuffer;
+}
+
+SerialStream &SerialStream::operator<< (unsigned long anULong) {
+    return (*this) << long (anULong);
+}
+
+SerialStream &SerialStream::operator<< (float aFloat) {
+    dtostrf (aFloat, 0, 10, conversionBuffer);
+    return (*this) << conversionBuffer;
+}
+
+const char endl = '\n';
+
+// ====== Timers
+
+static long Timer::currentTime = 0;
+
+Timer::Timer () {
+    resetIf (true);
+}
+
+static void Timer::tick () {
+    currentTime = millis ();
+}
+
+void Timer::resetIf (bool condition) {
+    if (condition) {
+        startTime = currentTime;
+    }
+}
+
+Timer::operator int () {
+    return currentTime - startTime;
+}
+
+// ====== Oneshots
+
+Oneshot::Oneshot () {
+    state = 0;
+}
+
+void Oneshot::triggerIf (bool condition) {
+    state <<= 1;
+    if (condition) {
+        state |= 1;
+    }
+}
+
+Oneshot::operator bool () {
+    return (state & 3) == 1;
+}
+
+}
+
+// ====== Main
+
+void setup () {
+    Serial.begin (9600);
+    __main__ ();
+}
+
+void loop () {
+    return;
+}
+
+
---
+++ cantino.h
@@ -0,0 +1,85 @@
+/*
+====== Legal notices
+
+Copyright 2017 Jacques de Hooge, GEATEC engineering, www.geatec.com
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+  http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+#pragma once
+
+namespace cantino {
+
+// ====== Streams
+
+struct SerialStream {
+    HardwareSerial *serial;
+    char conversionBuffer [16];
+
+    SerialStream (HardwareSerial &serial);
+    
+    SerialStream &operator>> (char * const chars);
+    SerialStream &operator>> (bool &aBool);
+    SerialStream &operator>> (char &aChar);
+    SerialStream &operator>> (unsigned char &anUChar);
+    SerialStream &operator>> (int &anInt);
+    SerialStream &operator>> (unsigned int &anUInt);
+    SerialStream &operator>> (long &aLong);
+    SerialStream &operator>> (unsigned long &anULong);
+    SerialStream &operator>> (float &aFloat);
+    
+    SerialStream &operator<< (const char * const chars);
+    SerialStream &operator<< (bool aBool);
+    SerialStream &operator<< (char aChar);
+    SerialStream &operator<< (unsigned char anUChar);
+    SerialStream &operator<< (int anInt);
+    SerialStream &operator<< (unsigned int anUInt);
+    SerialStream &operator<< (long aLong);
+    SerialStream &operator<< (unsigned long aULong);
+    SerialStream &operator<< (float aFloat);
+};
+
+extern const char endl;
+
+// ====== Timers
+
+struct Timer {
+    static long currentTime;
+    long startTime;
+
+    Timer ();
+    static void tick ();
+    void resetIf (bool condition);
+    operator int ();
+};
+
+// ====== Oneshots
+
+struct Oneshot {
+    byte state;
+
+    Oneshot ();
+    void triggerIf (bool condition);
+    operator bool ();
+};
+
+}
+
+// ====== Main
+
+extern void setup ();
+extern void loop ();
+
+#define main __main__
+extern int main ();
+
