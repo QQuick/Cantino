f8d90b27284d015eef8e467e583b8521e25a2b3b
JdeH <info@qquick.org>
2017-10-05 16:24:01 +0200

    Until control_loop

---
+++ lessons/lesson_2_repetition/control_loop/control_loop.ino
@@ -0,0 +1,25 @@
+#include <cantino.h>
+using namespace cantino;
+
+int main () {
+    pinMode (LED_BUILTIN, OUTPUT);
+    bool again = true;
+    
+    while (again) { // Control loop
+        for (int i = 0; i < 3; i++) {
+            digitalWrite (LED_BUILTIN, true);
+            delay (500);
+            digitalWrite (LED_BUILTIN, false);
+            delay (500);
+        }
+        
+        cout << "Blink again <y/n>? ";
+        char answer;
+        cin >> answer;
+        again = answer == 'y';
+    }
+    
+    cout << "Program terminated" << endl;
+    return 0;
+}
+
--- lessons/lesson_2_repetition/tables3/tables3.ino
+++ lessons/lesson_2_repetition/tables3/tables3.ino
@@ -3,27 +3,21 @@ using namespace cantino;
 
 int main () {   
     cout << "Type 0 to quit!" << endl;
-    
-    for (int tableNr = 1; tableNr <= 10; tableNr++) {
-        for (int lineNr = 2; lineNr <= 10; lineNr++) {
-            int answer = -1;
-
-            do {
-                cout << lineNr << " x " << tableNr << " = ";
-                
-                if (answer == 0) {
-                    break;
-                }
-                
-                cin >> answer;
-            } while (answer != lineNr * tableNr);
-            
-            if (answer == 0) {
-                break;
+        for (int tableNr = 1; tableNr <= 10; tableNr++) {
+            for (int lineNr = 1; lineNr <= 10; lineNr++) {
+                int answer;
+                do {
+                    cout << lineNr << " x " << tableNr << " = ";
+                    cin >> answer;
+                    if (answer == 0) {
+                        cout << "Program quit by user" << endl;
+                        return 0;
+                    }
+                } while (answer != lineNr * tableNr);
             }
+            cout << endl;
         }
-        cout << endl;
-    }
+    cout << "Program terminated" << endl;
     return 0;
 }
 
