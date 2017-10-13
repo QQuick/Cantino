10ff7e9cc6bcb2a345a28da23871110a7228dee7
JdeH <info@qquick.org>
2017-10-13 17:01:30 +0200

    Servo now also works with standard servo lib, second example added

--- examples/servo_test_alternative_lib/servo_test_alternative_lib.ino
+++ examples/servo_test_alternative_lib/servo_test_alternative_lib.ino
@@ -9,26 +9,30 @@ int main () {
     int limits[2] = {30,150};    // set limitations (min/max: 0->180)
     boolean refresh = false;    // toggle refresh on/off
 
-    SoftwareServo myservo;  // create servo object to control a servo 
+    SoftwareServo servo;  // create servo object to control a servo 
 
     // attaches the servo on pin to the servo object
-    myservo.attach(pinServo);  
+    servo.attach (pinServo);  
 
     // init angle of servo inbetween two limitations
-    myservo.write((limits[1]-limits[0])/2); 
+    servo.write ((limits [1] - limits [0]) / 2); 
 
     while (true) {
         // refresh angle
-        int angle = myservo.read();
+        int angle = servo.read ();
     
         // change direction when limits
-        if (angle >= limits[1] || angle <= limits[0])  speed = -speed;
+        if (angle >= limits [1] || angle <= limits [0]) {
+            speed = -speed;
+        }
     
-        myservo.write(angle + speed);   
+        servo.write (angle + speed);   
     
         // set refresh one time / 2
-        refresh = refresh ? false : true;
-        if (refresh) SoftwareServo::refresh();
+        refresh = !refresh;
+        if (refresh) {
+            SoftwareServo::refresh ();
+        }
     
         cout << "Angle: " << angle << endl;
     }
---
+++ examples/servo_test_default_lib/servo_test_default_lib.ino
@@ -0,0 +1,50 @@
+#include <cantino.h>
+#include <Servo.h>
+
+using namespace cantino;
+
+int main () {
+    int const servoPin = 8;
+     
+    Servo servo;  
+     
+    int servoAngle = 0;   // servo position in degrees
+    
+    Serial.begin(9600);  
+    servo.attach (servoPin);
+
+ 
+    while (true) {
+        //control the servo's direction and the position of the motor
+        servo.write (45);      // Turn SG90 servo Left to 45 degrees
+        delay(1000);          // Wait 1 second
+        servo.write(90);      // Turn SG90 servo back to 90 degrees (center position)
+        delay(1000);          // Wait 1 second
+        servo.write(135);     // Turn SG90 servo Right to 135 degrees
+        delay(1000);          // Wait 1 second
+        servo.write(90);      // Turn SG90 servo back to 90 degrees (center position)
+        delay(1000);
+        //end control the servo's direction and the position of the motor
+
+
+       //control the servo's speed  
+       //if you change the delay value (from example change 50 to 10), the speed of the servo changes
+        for (servoAngle = 0; servoAngle < 180; servoAngle++) { //move the micro servo from 0 degrees to 180 degrees
+            servo.write (servoAngle);              
+            delay(50);                  
+        }
+    
+        for (servoAngle = 180; servoAngle > 0; servoAngle--) { //now move back the micro servo from 0 degrees to 180 degrees                                
+            servo.write (servoAngle);          
+            delay(10);      
+        }
+        //end control the servo's speed
+
+        for (int i = 0; i < 5; i++) {
+            cout<< "Give angle: ";
+            cin >> servoAngle;
+            servo.write (servoAngle);
+        }
+    }
+    return 0;
+}
